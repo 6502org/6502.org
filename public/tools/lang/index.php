afa47808e3a6ba5c2f58faa2fda0dbe0a887e9bd
Mike Naberezny <mike@naberezny.com>
2013-07-15 20:55:23 -0700

    Use a consistent domain name

--- public/tools/lang/index.php
+++ public/tools/lang/index.php
@@ -4,7 +4,7 @@ filesize("../../header.html"));
 echo($file_content);
 ?>
 
-<TITLE>www.6502.org: Compilers & Languages</TITLE>
+<TITLE>6502.org: Compilers & Languages</TITLE>
 <META name="description" content="High-level language compilers and
 interpreters for the 6502 microprocessor.">
 
@@ -38,10 +38,10 @@ of Small C, this is a freeware C compiler for 6502-based systems from
 Ullrich von Bassewitz.</FONT></FONT>
 
 <LI><A href="lcc-1.9-retargetable.tar.gz">LCC 65C816 [lcc-1.9-retargetable.tar.gz]</a> - Toshi Morita has
-successfully ported LCC to compile for the 65C816 microprocessor, and this archive contains complete source code for the 
+successfully ported LCC to compile for the 65C816 microprocessor, and this archive contains complete source code for the
 compiler along with simple instructions and examples.</li>
 
-<FONT COLOR=RED><LI><FONT COLOR=BLACK><A HREF="http://www.kdef.com/geek/vic/quetz.html">Quetzalcoatl</A> - Brendan Jones created this C compiler which runs under Linux and Win32.  
+<FONT COLOR=RED><LI><FONT COLOR=BLACK><A HREF="http://www.kdef.com/geek/vic/quetz.html">Quetzalcoatl</A> - Brendan Jones created this C compiler which runs under Linux and Win32.
 You can download the program from this site, along with an HTML programming manual, and the source code is available by request.</FONT></FONT>
 </UL>
 
