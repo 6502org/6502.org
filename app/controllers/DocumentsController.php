8e93bb92950b0a4e6a16664cd293b4451fb26f6f
Mike Naberezny <mike@naberezny.com>
2015-05-24 15:39:05 -0700

    Use framework redirectTo() instead of PHP functions

--- app/controllers/DocumentsController.php
+++ app/controllers/DocumentsController.php
@@ -9,23 +9,23 @@ class DocumentsController extends ApplicationController
                              $spec['username'],
                              $spec['password']);
 
-	    $keyList = explode('/', trim($this->params['path'], '/'));
+        $keyList = explode('/', trim($this->params['path'], '/'));
         foreach($keyList as $k => $v) {
             if (empty($v)) { unset($keyList[$k]); }
         }
         array_unshift($keyList, 'documents');
 
-      /* Welcome message */
-    	$welcome = "Documents Archive";
-    	$title = "Documents Archive";
-    	$description = "A collection of useful documents pertaining to the 6502 microprocessor.";
+        /* Welcome message */
+        $welcome = "Documents Archive";
+        $title = "Documents Archive";
+        $description = "A collection of useful documents pertaining to the 6502 microprocessor.";
 
-      /* Validate all keys by retrieving the folder information for
-         each key from the database.  If an invalid key is found, redirect the URL
-         using the preceeding known-good keys.  */
+        /* Validate all keys by retrieving the folder information for
+           each key from the database.  If an invalid key is found, redirect the URL
+           using the preceeding known-good keys.  */
         $this->folders = array();
-    	$url = "/";
-    	foreach($keyList as $key) {
+        $url = "/";
+        foreach($keyList as $key) {
             if (!empty($this->folders)) {
                 $parent_folder_id = $this->folders[count($this->folders)-1]['id'];
             } else {
@@ -41,14 +41,14 @@ class DocumentsController extends ApplicationController
                                  'parent_folder_id' => $parent_folder_id));
             $folder = $stmt->fetch();
 
-    		if (!empty($folder)) {
-    			$url .= $folder['slug'] . '/';
-    			$folder['url'] = $url;
-    			array_push($this->folders, $folder);
-    		} else {
-    			/* If the key does not exist, it might also be a filename.  If it is,
-    			   update the download counter and redirect.  */
-    			if (count($this->folders) > 0) {
+            if (!empty($folder)) {
+                $url .= $folder['slug'] . '/';
+                $folder['url'] = $url;
+                array_push($this->folders, $folder);
+            } else {
+                /* If the key does not exist, it might also be a filename.  If it is,
+                   update the download counter and redirect.  */
+                if (count($this->folders) > 0) {
                     $sql = 'SELECT *
                             FROM document_files
                             WHERE (filename = :key) AND (folder_id = :id)
@@ -58,33 +58,36 @@ class DocumentsController extends ApplicationController
                                          'id'  => $this->folders[count($this->folders)-1]['id']));
                     $doc = $stmt->fetch();
 
-    				if (empty($doc)) {
-    					// If the filename could not be found, redirect to last known-good keys.
-    					header ("Location: $url");
-    					exit();
-    				} else {
-    					// If the filename was found, update the download counter and redirect to download.
+                    if (empty($doc)) {
+                        // If the filename could not be found, redirect to last known-good keys.
+                        $this->redirectTo($url);
+                        return;
+                    } else {
+                        // If the filename was found, update the download counter and redirect to download.
                         $sql = 'UPDATE document_files
                                 SET downloads = downloads + 1
                                 WHERE id = :id';
                         $stmt = $this->pdo->prepare($sql);
                         $stmt->execute(array('id' => $doc['id']));
 
-    					header ("Location: http://archive.6502.org/" . $this->folders[count($this->folders)-1]['path'] . $doc['filename']);
-    					exit();
-    				}
-    			} else {
-    				// Last key was not a valid key or filename.  Redirect to last known-good keys.
-    				header ("Location: $url");
-    				exit();
-    			}
-    		}
-    	}
+                        $url = "http://archive.6502.org/" .
+                               $this->folders[count($this->folders)-1]['path'] .
+                               $doc['filename'];
+                        $this->redirectTo($url);
+                        return;
+                    }
+                } else {
+                    // Last key was not a valid key or filename.  Redirect to last known-good keys.
+                    $this->redirectTo($url);
+                    return;
+                }
+            }
+        }
 
-        /* Get subfolders of the current folder */
-      	$this->myFolder = $this->folders[count($this->folders)-1];
+        // Get subfolders of the current folder
+        $this->myFolder = $this->folders[count($this->folders)-1];
 
-      	$myId = $this->folders[count($this->folders)-1]['id'];
+        $myId = $this->folders[count($this->folders)-1]['id'];
 
         $sql="SELECT * FROM document_folders
               WHERE parent_folder_id = :id
@@ -93,21 +96,21 @@ class DocumentsController extends ApplicationController
         $stmt->execute(array('id' => $myId));
         $this->myFolders = $stmt->fetchAll();
 
-    	for ($i=0; $i<count($this->myFolders); $i++) {
-    		$this->myFolders[$i]['url'] = $url . $this->myFolders[$i]['slug'] . '/';
-    	}
+        for ($i=0; $i<count($this->myFolders); $i++) {
+            $this->myFolders[$i]['url'] = $url . $this->myFolders[$i]['slug'] . '/';
+        }
 
         $sql="SELECT * FROM document_files
               WHERE folder_id = :id
               ORDER BY sort_title, title ASC";
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute(array('id' => $myId));
-		$this->myFolder['docs'] = $stmt->fetchAll();
+        $this->myFolder['docs'] = $stmt->fetchAll();
 
         $this->_updateFileSizes();
         $this->_updatePageCounts();
 
-        /* Create URL for each doc */
+        // Create URL for each doc
         foreach ($this->myFolder['docs'] as &$doc) {
             $doc['url'] = $url . $doc['filename'];
         }
@@ -118,14 +121,14 @@ class DocumentsController extends ApplicationController
     protected function _updateFileSizes()
     {
         foreach ($this->myFolder['docs'] as &$doc) {
-    		if ($doc['filesize'] == 0) {
+            if ($doc['filesize'] == 0) {
                 $filespec = dirname(MAD_ROOT)
                           . '/archive.6502.org/public/'
                           . $this->myFolder['path']
                           . $doc['filename'];
 
-    	  		if (file_exists($filespec)) {
-    		  		$doc['filesize'] = filesize($filespec);
+                if (file_exists($filespec)) {
+                    $doc['filesize'] = filesize($filespec);
 
                     $sql = 'UPDATE document_files
                             SET filesize = :filesize
@@ -133,9 +136,9 @@ class DocumentsController extends ApplicationController
                     $stmt = $this->pdo->prepare($sql);
                     $stmt->execute(array('filesize' => $doc['filesize'],
                                          'id'       => $doc['id']));
-    	  		}
-    		}
-    	}
+                }
+            }
+        }
     }
 
     // Update PDF page counts of any file whose count is 0
