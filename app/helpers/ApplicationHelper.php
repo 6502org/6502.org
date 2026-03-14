9b55aa69711b898d0cdb5d5f8b426f66efd9a58a
Mike Naberezny <mike@naberezny.com>
2026-03-13 17:48:41 -0700

    Use a new responsive layout for better viewing on mobile

--- app/helpers/ApplicationHelper.php
+++ app/helpers/ApplicationHelper.php
@@ -2,4 +2,15 @@
 
 class ApplicationHelper extends Mad_View_Helper_Base
 {
+    public function milestoneYears()
+    {
+        $foundedYear = 1999;
+        $foundedMonth = 6;
+        $now = getdate();
+        $years = $now['year'] - $foundedYear;
+        if ($now['mon'] < $foundedMonth) {
+            $years--;
+        }
+        return floor($years / 5) * 5;
+    }
 }
