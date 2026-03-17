e7ae249faeb919f911836c61827702e127229de5
Mike Naberezny <mike@naberezny.com>
2026-03-16 17:00:17 -0700

    Fix PHP error when running migrations with no args

--- vendor/Mad/Task/BuiltinSet/Database.php
+++ vendor/Mad/Task/BuiltinSet/Database.php
@@ -24,7 +24,7 @@ class Mad_Task_BuiltinSet_Database extends Mad_Task_Set
         try {
             $script  = array_shift($GLOBALS['argv']);
             $task    = array_shift($GLOBALS['argv']);
-            $version = array_shift($GLOBALS['argv']);
+            $version = array_shift($GLOBALS['argv']) ?? '';
             $ver = preg_match('/VERSION=(\d*)/', $version, $match) ? $match[1] : null;
 
             Mad_Model_Migration_Migrator::migrate(MAD_ROOT."/db/migrate/", $ver);
